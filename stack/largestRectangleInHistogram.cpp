2d1397ccb95ad56f3f77c2ae8b94a25066be996d
Nitesh Kohli <celestialstark007@gmail.com>
2025-12-20 06:01:21 +0000

    added largest rectangle in histogram

---
+++ stack/largestRectangleInHistogram.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int ele = st.top();
+                st.pop();
+                //it's NSE is the ith value
+                //it's PSE is the st.top if st.empty() false else -1
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
+                int area = heights[ele] * (nse - pse - 1);
+                maxArea = max(maxArea, area);
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int ele = st.top();
+            st.pop();
+            int nse = n;
+            int pse = st.empty() ? -1 : st.top();
+            maxArea = max(maxArea, heights[ele] * (nse - pse - 1));
+        }
+
+        return maxArea;
+    }
+};
