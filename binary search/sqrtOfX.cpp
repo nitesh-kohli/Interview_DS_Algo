9861d192c0bd705ab19124f361eb07a02619f643
Nitesh Kohli <celestialstark007@gmail.com>
2025-12-21 07:29:04 +0000

    added sqrt of x problem

---
+++ binary search/sqrtOfX.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 1 || x == 0) return x;
+        int low = 1, high = x/2;
+
+        while(low <= high){
+            long long mid = low + (high-low)/ 2;
+            if(mid*mid == x) return mid;
+            else if(mid*mid < x) low = mid + 1;
+            else high = mid - 1;
+        }
+        return high;
+    }
+};
